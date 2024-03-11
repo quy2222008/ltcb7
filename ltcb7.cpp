15a79e779382bfe21a7f2a651eeb9399967ec663
quy2222008 <voquocquy23032022@gmail.com>
2024-03-11 11:21:29 +0700

    Add project files.

---
+++ ltcb7.cpp
@@ -0,0 +1,22 @@
+// ltcb7.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+using namespace std;
+int main() {
+	int so1, so2, so3, lonnhat, nhonhat;
+	cout << " nhap so1:";
+	cin >> so1;
+
+	cout << "nhap so2";
+	cin >> so2;
+	 
+	cout << "nhapso3";
+	cin >> so3;
+
+	lonnhat = nhonhat = s1;
+	 
+	if (nhonhat>so2 ){
+		nhonhat
+
+
